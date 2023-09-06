c4551d8bdd570b4d767d4061317c0d1ea8851e85
ameen <ameen55668@gmail.com>
2023-09-06 09:07:33 +0500

    iniialize react ntive nevigation

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './Login';
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
